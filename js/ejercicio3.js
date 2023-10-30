7c03fb5b9a7ab387a0208d4734b7ae2cb115ac03
Leonel Ramella <leonelramella30@gmail.com>
2023-10-30 01:15:39 -0300

    Resuelto

---
+++ js/ejercicio3.js
@@ -0,0 +1,4 @@
+const gradosCelcius = 100;
+const gradosFahrenheit = (100 * 9/5) + 32;
+console.log("grados en Celcius" + gradosCelcius)
+console.log ("Se convertio esos grados a fahrenheit"+gradosFahrenheit);
